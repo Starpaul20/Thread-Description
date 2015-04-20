21e5aa6c12cfad050cc5e1d79ad3d90771c637ab
PaulBender <paulbender@live.com>
2015-04-20 17:08:46 -0400

    Bump to version v1.1

--- inc/plugins/threaddescription.php
+++ inc/plugins/threaddescription.php
@@ -82,7 +82,7 @@ function threaddescription_info()
 		"website"			=> "http://galaxiesrealm.com/index.php",
 		"author"			=> "Starpaul20",
 		"authorsite"		=> "http://galaxiesrealm.com/index.php",
-		"version"			=> "1.0",
+		"version"			=> "1.1",
 		"codename"			=> "threaddescription",
 		"compatibility"		=> "18*"
 	);
